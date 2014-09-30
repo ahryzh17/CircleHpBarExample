8ed280a8957ccc0a565d27170cb09f3580daacf1
geniikw <geniikw@gmail.com>
2014-09-30 10:36:05 +0900

    CirCleHpExample

---
+++ Assets/BindCircle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BindCircle : MonoBehaviour {
+
+    [SerializeField]
+    Color start;
+    [SerializeField]
+    Color end;
+
+    [SerializeField]
+    Image Circle;
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Circle.material.SetFloat("_Angle", Mathf.Lerp(-3.14f, 3.14f, scrollbar.value));
+        Circle.material.SetColor("_Color", Color.Lerp(start, end, scrollbar.value));
+	}
+}
