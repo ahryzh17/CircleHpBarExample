c8cd9a215315d2d979872a4ad4e634036a715c65
geniikw <geniikw@gmail.com>
2014-10-04 14:45:05 +0900

    Update New method
    but it has a bug...
    when progress value become 0
    circle is disappear

---
+++ Assets/BindCircle2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Scrollbar))]
+public class BindCircle2 : MonoBehaviour
+{
+    [SerializeField]
+    Image CircleImage;
+    [SerializeField]
+    Color start;
+    [SerializeField]
+    Color end;
+
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+    void Update()
+    {
+        CircleImage.fillAmount = scrollbar.value;
+        CircleImage.color = Color.Lerp(start, end, scrollbar.value);
+    }
+
+     
+}
